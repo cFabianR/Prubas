7eaa4781a45faaddfbf33e0445363f740e88f2f0
Fabian <fabian@localhost.localdomain>
2020-05-18 14:27:26 -0500

    Updates

---
+++ precache-manifest.ee81b44b3ead9e8216565d844e4f6c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb870652c4de3143ea650d185f0044f",
+    "url": "/Pruebas/index.html"
+  },
+  {
+    "revision": "c920610ccae283b11bb2",
+    "url": "/Pruebas/static/css/main.7148a500.chunk.css"
+  },
+  {
+    "revision": "09710bb3071414e32e90",
+    "url": "/Pruebas/static/js/2.c38afd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pruebas/static/js/2.c38afd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c920610ccae283b11bb2",
+    "url": "/Pruebas/static/js/main.4782b4d6.chunk.js"
+  },
+  {
+    "revision": "2e33b01fe7af628fd461",
+    "url": "/Pruebas/static/js/runtime-main.18f0175a.js"
+  }
+]);
